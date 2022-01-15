4f990191eb9bcb7e4679a6c0239090af93fc997a
Nico Korthout <nicokorthout@gmail.com>
2022-01-15 11:45:49 +0100

    test(1): create and merge a pr

--- test/case1.sh
+++ test/case1.sh
@@ -1,3 +1,36 @@
 #! /bin/bash
 
-echo "Hello World!"
+# When run this test will:
+# - create a branch from main as backport target
+# - create a branch from main for new changes
+# - add a commit to new
+# - open a pull request to merge it to main
+# - merge the pull request
+# - see the simle.yml workflow run on pull_request[closed]
+# - check that pull request is opened to target with cherrypicked commit
+# - cleanup: revert merge to main, close backport-pr and delete both new branches
+
+git branch case1-backport-target
+git push -u origin case1-backport-target
+
+git branch case1-new-changes
+git checkout case1-new-changes
+
+mkdir case1
+echo "A changed line is added" >> case1/file1
+git commit -am "case(1): add changed line"
+git push -u origin case1-new-changes
+
+gh pr create \
+  --head case1-new-changes \
+  --base main \
+  --title "Case(1): Add a changed line" \
+  --body "Adds a changed line"
+
+gh pr merge \
+  --merge
+  --subject "case(1): merge pull request"
+
+# wait for workflow to finish
+# check that pull request is opened to target with cherrypicked commit
+# cleanup
